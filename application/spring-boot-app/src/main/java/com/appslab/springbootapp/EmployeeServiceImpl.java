80469831a53da2d276f919e54e055855d13385aa
exectionik <exectionik9@gmail.com>
2020-12-14 20:19:25 +0100

    This amazing pile of code is solving problem n02

--- application/spring-boot-app/src/main/java/com/appslab/springbootapp/EmployeeServiceImpl.java
+++ application/spring-boot-app/src/main/java/com/appslab/springbootapp/EmployeeServiceImpl.java
@@ -2,23 +2,31 @@ package com.appslab.springbootapp;
 
 import com.appslab.springbootapp.Employee;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.List;
+
 @Service
+
 public class EmployeeServiceImpl implements EmployeeService {
     int number = 0;
     @Override
+
     public int writeNumber(){
+
         return ++number;
     }
     @Override
+
     public float sumOfSalaries(List<Employee> list) {
         return (float) list.stream().mapToDouble(n -> n.getSalary()).sum();
 
     }
 
     @Override
+
     public int sumOfBonuses(List<Employee> list) {
         return list.stream().mapToInt(n -> n.getBonus()).sum();
     }
