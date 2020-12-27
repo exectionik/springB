4b7b8c4b1101431fa84eea27019cf8d86046baef
exectionik <exectionik9@gmail.com>
2020-12-27 21:56:25 +0100

    toto by mohlo byt riesenie prvej ulohy z databaz

---
+++ application/spring-boot-app/src/main/java/com/appslab/springbootapp/Company.java
@@ -0,0 +1,18 @@
+package com.appslab.springbootapp;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Company {
+    @Id
+    String name;
+    public Company(String name){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
--- application/spring-boot-app/src/main/java/com/appslab/springbootapp/Driver.java
+++
@@ -1,8 +0,0 @@
-package com.appslab.springbootapp;
-
-public class Driver extends Employee {
-    public Driver(float salary,int bonus){
-
-        super(salary,bonus, Employees_list.DRIVER);
-    }
-}
